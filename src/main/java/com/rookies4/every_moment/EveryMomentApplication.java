b823cbd6fc4a6d7f2efb11c7b9e15adb96604fbe
hia1394 <a4737465@naver.com>
2025-09-05 15:23:30 +0900

    1st

---
+++ src/main/java/com/rookies4/every_moment/EveryMomentApplication.java
@@ -0,0 +1,13 @@
+package com.rookies4.every_moment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryMomentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryMomentApplication.class, args);
+	}
+
+}
